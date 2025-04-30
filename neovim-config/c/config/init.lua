f9632a381e6dd7dc922c78c7c2c54160905ab515
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-04-29 23:30:59 -0300

    chore: initial commit

---
+++ neovim-config/c/config/init.lua
@@ -0,0 +1,34 @@
+-- nvim-configs/c
+local vim = vim
+local Plug = vim.fn['plug#']
+vim.o.exrc = true
+
+vim.call('plug#begin')
+Plug('diepm/vim-rest-console')
+Plug('dense-analysis/ale')
+Plug('preservim/nerdtree')
+Plug('nvim-lua/plenary.nvim')
+Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' })
+vim.call('plug#end')
+
+vim.cmd([[
+set nocompatible            " disable compatibility to old-time vi
+set showmatch               " show matching 
+set hlsearch                " highlight search 
+set incsearch               " incremental search
+set tabstop=4               " number of columns occupied by a tab 
+set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
+set expandtab               " converts tabs to white space
+set shiftwidth=4            " width for autoindents
+set autoindent              " indent a new line the same amount as the line just typed
+set number                  " add line numbers
+set wildmode=longest,list   " get bash-like tab completions
+set cc=180                  " set an 80 column border for good coding style
+filetype plugin indent on   "allow auto-indenting depending on file type
+syntax on                   " syntax highlighting
+set mouse=a                 " enable mouse click
+set clipboard=unnamedplus   " using system clipboard
+filetype plugin on
+set cursorline              " highlight current cursorline
+]])
+
